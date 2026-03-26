5c7d9d63b0ec7465c91e6578c06ffb72193e9fd1
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2026-03-26 19:38:23 +0300

    setup Celery

--- config/settings.py
+++ config/settings.py
@@ -147,3 +147,18 @@ TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
+
+CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
+
+# Celery Beat schedule
+CELERY_BEAT_SCHEDULE = {
+    'send_habit_reminders': {
+        'task': 'telegram.tasks.send_habit_reminders',
+        'schedule': 60.0,  # Каждую минуту
+    },
+}

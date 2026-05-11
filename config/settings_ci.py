f358c3abbb89930eafbd90269325897f8a58b802
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2026-05-11 11:27:38 +0300

    fixed error

---
+++ config/settings_ci.py
@@ -0,0 +1,18 @@
+from .settings import *
+
+# Принудительно используем SQLite для CI
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/tmp/test_db.sqlite3',
+    }
+}
+
+# Отключаем брокеры для CI
+CELERY_TASK_ALWAYS_EAGER = True
+CELERY_BROKER_URL = 'memory://'
+CELERY_RESULT_BACKEND = 'cache'
+
+# Упрощаем для CI
+DEBUG = True
+SECRET_KEY = 'ci-test-secret-key'
